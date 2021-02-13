b518f71b6c92df8727817b5dc0dad073c2679e38
Thiago Lira <lira_thiago@msn.com>
2021-02-13 14:55:41 -0300

    Refactor search input tests

--- tests/unit/search-input.spec.js
+++ tests/unit/search-input.spec.js
@@ -3,23 +3,24 @@ import { shallowMount } from '@vue/test-utils';
 import SearchInput from '@/components/SearchInput.vue';
 
 describe('SearchInput.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(SearchInput);
+  });
+
   it('should to render', () => {
-    const wrapper = shallowMount(SearchInput);
     expect(wrapper.exists()).to.equal(true);
   });
 
   it('should allow to set an optionally placeholder', async () => {
-    const wrapper = shallowMount(SearchInput, {
-      propsData: {
-        placeholder: 'Procure por heróis',
-      },
+    await wrapper.setProps({
+      placeholder: 'Procure por heróis',
     });
     const input = wrapper.find('input');
     expect(input.element.placeholder).to.equal('Procure por heróis');
   });
 
   it('should to pass input value when "input" event is emitted', async () => {
-    const wrapper = shallowMount(SearchInput);
     const input = wrapper.find('input');
     input.element.value = 'A';
     input.trigger('keyup');
