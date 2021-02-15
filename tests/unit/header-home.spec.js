2daf791636ee018dc332a59cd3a1a70e1088f9ed
Thiago Lira <lira_thiago@msn.com>
2021-02-15 02:09:14 -0300

    Fix HeaderHome test

--- tests/unit/header-home.spec.js
+++ tests/unit/header-home.spec.js
@@ -1,10 +1,9 @@
-import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import HeaderHome from '@/components/HeaderHome.vue';
 
 describe('HeaderHome.vue', () => {
   it('should to exist', () => {
     const wrapper = shallowMount(HeaderHome);
-    expect(wrapper.exists()).to.equal(true);
+    expect(wrapper.exists()).toEqual(true);
   });
 });
