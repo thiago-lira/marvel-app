9511a3f2471bdaad3e7e47cd0cf7fae7cf0dd63a
Thiago Lira <lira_thiago@msn.com>
2021-02-20 22:34:25 -0300

    #15 - create a service that fetch a specific character

--- tests/unit/marvel-service.spec.js
+++ tests/unit/marvel-service.spec.js
@@ -4,18 +4,18 @@ import marvelService from '@/services/marvel';
 jest.mock('@/utils/http');
 
 describe('Marvel service', () => {
+  beforeEach(() => {
+    const fakeResponse = new Promise((resolve) => {
+      resolve({ data: {} });
+    });
+    http.get.mockReturnValue(fakeResponse);
+  });
+
+  afterEach(() => {
+    http.get.mockReset();
+  });
+
   describe('getCharacters', () => {
-    beforeEach(() => {
-      const fakeResponse = new Promise((resolve) => {
-        resolve({ data: {} });
-      });
-      http.get.mockReturnValue(fakeResponse);
-    });
-
-    afterEach(() => {
-      http.get.mockReset();
-    });
-
     it('should to return a promise', () => {
       const result = marvelService.getCharacters();
       expect(result instanceof Promise).toBe(true);
@@ -28,11 +28,26 @@ describe('Marvel service', () => {
           expect(data).toEqual(expectedData);
         });
     });
+  });
 
+  describe('getCharactersByName', () => {
     it('should to call api with a name to search', () => {
       marvelService.getCharactersByName('Hulk');
       const { nameStartsWith } = http.get.mock.calls[0][1].params;
       expect(nameStartsWith).toEqual('Hulk');
+    });
+  });
+
+  describe('getCharacterById', () => {
+    it('should to return a promise', () => {
+      const promise = marvelService.getCharacterById(1017325);
+      expect(promise instanceof Promise).toBe(true);
+    });
+
+    it('should to fetch a character passing id', () => {
+      const characterId = 1017325;
+      marvelService.getCharacterById(characterId);
+      expect(http.get.mock.calls[0][0]).toContain(`characters/${characterId}`);
     });
   });
 });
