9cf8b59c7e7c1c5907c3d6e8f1442382cac13f06
Thiago Lira <lira_thiago@msn.com>
2021-02-28 03:22:37 -0300

    #12 - fix some testing warnings

--- tests/unit/logo-app.spec.js
+++ tests/unit/logo-app.spec.js
@@ -1,9 +1,15 @@
+import Vue from 'vue';
 import { shallowMount } from '@vue/test-utils';
 import LogoApp from '@/components/LogoApp.vue';
 
 describe('LogoApp.vue', () => {
   it('should to render', () => {
-    const wrapper = shallowMount(LogoApp);
+    const RouterLink = Vue.component('router-link', { template: '' });
+    const wrapper = shallowMount(LogoApp, {
+      components: {
+        RouterLink,
+      },
+    });
     expect(wrapper.exists()).toBe(true);
   });
 });
