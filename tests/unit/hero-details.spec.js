d84cefe825946b829e415d8fc84e1d362de541f8
Thiago Lira <lira_thiago@msn.com>
2021-02-28 00:44:34 -0300

    #5 - fix tests. Closes #6

--- tests/unit/hero-details.spec.js
+++ tests/unit/hero-details.spec.js
@@ -3,7 +3,12 @@ import HeroDetails from '@/components/HeroDetails.vue';
 
 describe('HeroDetails.vue', () => {
   it('should to render', () => {
-    const wrapper = shallowMount(HeroDetails);
+    const wrapper = shallowMount(HeroDetails, {
+      propsData: {
+        character: { name: 'Spider-Man', description: 'É o piter parque', image: '/path/to/image.jpg' },
+        comics: [],
+      },
+    });
 
     expect(wrapper.exists()).toBe(true);
   });
