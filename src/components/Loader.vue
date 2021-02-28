19c149424e231e2c31cecf8c640c4c5fecff033d
Thiago Lira <lira_thiago@msn.com>
2021-02-28 03:50:37 -0300

    #12 - Fix Loader component animation

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -11,3 +11,28 @@ export default {
   name: 'Loader',
 };
 </script>
+
+<style lang="scss" scoped>
+.loader {
+  align-items: center;
+  height: 72px;
+
+  &-content img {
+    animation: loading 1s infinite ease-in-out;
+  }
+}
+
+@keyframes loading {
+  0% {
+    height: 72px;
+  }
+
+  50% {
+    height: 45px;
+  }
+
+  100% {
+    height: 72px;
+  }
+}
+</style>
